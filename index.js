be36d625bb5b25f95719aea892fbba51e1f066f0
wagnerSfarias <wagnner@universalhost.com.br>
2023-03-30 11:34:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const server = express();
+const port = process.env.PORT || 3000
+const brands = require('./src/data/brands.json');
+const cars = require('./src/data/cars.json');
+
+server.get('/brands',(req, res)=>{
+    
+        return res.json(brands);
+
+})
+
+server.get('/cars',(req, res)=>{
+    const {id} = req.query;
+    
+    if(!id){
+        return res.status(400).json({error: 'Informe um ID valido'})
+    }else if(id>5){
+        return res.status(400).json({error: 'ID informado está invalido'})
+    }
+
+    const listCars = cars.filter( t=> t.idbrand ==id)
+
+    return res.json(listCars);
+})
+
+server.listen(port);
